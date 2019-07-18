393fa90a75b3e68052f38d6c1da3c632843fb401
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-07-18 14:55:56 +0300

    Add a component of the Palindrome Checker

---
+++ src/Components/PalindromeChecker/PalindromeChecker.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PalindromeChecker() {
+  return (
+    <div className="PalindromeChecker">
+        <h1>הצלחה</h1>
+    </div>
+  );
+}
+
+export default PalindromeChecker;
