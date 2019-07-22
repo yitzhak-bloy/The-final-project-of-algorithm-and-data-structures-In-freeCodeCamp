993ce292dd797e52141b9e895a2b08770288ad26
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-07-22 14:39:32 +0300

    Adding a website design

--- src/Components/checkerBox/checkerBox.jsx
+++ src/Components/checkerBox/checkerBox.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './checkerBox.css';
 
 function CheckerBox({ handleChange, handleClick }) {
   return (
     <div className="CheckerBox">
-        <input type="text" placeholder='Write a statement to check' onChange={handleChange} />
-        <input type="submit" value="check" onClick={handleClick} />
+        <input className="text" type="text" placeholder='Write a statement to check' onChange={handleChange} />
+        <input className="submit" type="submit" value="check" onClick={handleClick} />
     </div>
   );
 }
