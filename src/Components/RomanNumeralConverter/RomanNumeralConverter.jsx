fddac756eb048ec205a5b6f13c369361d9c3cbb9
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-08-01 23:33:57 +0300

    add RomanNumeralConverter

--- src/Components/RomanNumeralConverter/RomanNumeralConverter.jsx
+++ src/Components/RomanNumeralConverter/RomanNumeralConverter.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import CheckerBox from '../checkerBox/checkerBox';
 import { Link } from 'react-router-dom';
 
 class RomanNumeralConverter extends Component {
   constructor() {
       super();
       this.state = {
+        Number: ''
       };
     }
+
+  handleChange = (sentence) => {
+    this.setState({ Number: sentence.target.value })
+    console.log(this.state.Number)
+  }
 
   render() {
     return (
       <div className="RomanNumeralConverter">
           <h1>Roman Numeral Converter</h1>
+          <CheckerBox handleChange={this.handleChange} handleClick={this.handleClick} />
           <Link to='/'>HomePage</Link>
       </div>
     );
