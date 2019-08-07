4d073fe59e23059fb08e5090996a653b9767d716
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-08-07 22:36:43 +0300

    Adds a CheckerBox component to CaesarsCipher

--- src/Components/CaesarsCipher/CaesarsCipher.jsx
+++ src/Components/CaesarsCipher/CaesarsCipher.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import CheckerBox from '../checkerBox/checkerBox';
+
 // import CheckerBox from '../checkerBox/checkerBox';
 
 class  CaesarsCipher extends Component {
@@ -15,6 +17,12 @@ class  CaesarsCipher extends Component {
     return (
       <div className="CaesarsCipher">
         <h1>Caesars Cipher</h1>
+        <CheckerBox 
+          handleChange={this.handleChange} 
+          handleClick={this.handleClick} 
+          sentenceWithinTheInputFrame='Write a statement to convert'  
+          WordWrittenInAButton='convert' 
+        />
         <div>
           <Link to='/' >HomePage</Link>
         </div>
