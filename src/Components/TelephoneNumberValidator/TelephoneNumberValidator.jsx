13d67a271c2a0b803c0c09c8e2dc206b355f163b
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-08-13 11:58:01 +0300

    Completing the Telephone Number Validator component

--- src/Components/TelephoneNumberValidator/TelephoneNumberValidator.jsx
+++ src/Components/TelephoneNumberValidator/TelephoneNumberValidator.jsx
@@ -6,22 +6,48 @@ class  TelephoneNumberValidator extends Component {
   constructor() {
     super();
     this.state = {
-      Sentence: '',
-      result1: ''
+      phoneNumber: '',
+      result: ''
     };
+  }
+
+  handleChange = (phoneNumber) => {
+    this.setState({ phoneNumber: phoneNumber.target.value })
+  }
+
+  handleClick = () => {
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
+    this.setState({ result: regex.test(this.state.phoneNumber)}) 
   }
 
   render() {
     return (
       <div className="telephoneNumberValidator">
         <h1>telephone Number Validator</h1>
-      <CheckerBox 
-        handleChange={this.handleChange} 
-        handleClick={this.handleClick} 
-        sentenceWithinTheInputFrame='Write a number to verify'  
-        WordWrittenInAButton='verify' 
-      />
-      <Link to='/' className='link' >HomePage</Link>
+        <CheckerBox 
+          handleChange={this.handleChange} 
+          handleClick={this.handleClick} 
+          sentenceWithinTheInputFrame='Write a verification phone number'  
+          WordWrittenInAButton='validation' 
+        />
+        {
+          this.state.result
+          ?
+          <div>
+            <h1>This number is a real phone number!</h1>
+          </div>
+          : 
+            (
+              this.state.result === ''
+              ?       
+              <span>Enter a phone number for review</span>
+              : 
+              <h3>Sorry, this is not a real phone number. Try again.</h3>
+            )
+        }
+        <div>
+          <Link to='/' className='link' >HomePage</Link>
+        </div>
       </div>
     );
   }
