76442f56cae93482a17e402b14e38456c7689f41
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-08-23 16:12:32 +0300

    Add a caption to the currency input

--- src/Components/CurrencyAvailable/CurrencyAvailable.jsx
+++ src/Components/CurrencyAvailable/CurrencyAvailable.jsx
@@ -5,15 +5,15 @@ function CurrencyAvailable({ handlePenny, handleNickel, handleDime, handleQuarte
   return (
     <div>
         <h3>The amount of coins currently in the cash register</h3>
-        <input className="number" type="number" step="0.01" min="0" placeholder={'Penny'} onChange={handlePenny} />
-        <input className="number" type="number" step="0.05" min="0" placeholder={'Nickel'} onChange={handleNickel} />
-        <input className="number" type="number" step="0.1" min="0" placeholder={'Dime'} onChange={handleDime} />
-        <input className="number" type="number" step="0.25" min="0" placeholder={'Quarter'} onChange={handleQuarter} />
-        <input className="number" type="number" step="1" min="0" placeholder={'Dollar'} onChange={handleOne} />
-        <input className="number" type="number" step="5" min="0" placeholder={'Five Dollars'} onChange={handleFive} />
-        <input className="number" type="number" step="10" min="0" placeholder={'Ten Dollars'} onChange={handleTen} />
-        <input className="number" type="number" step="20" min="0" placeholder={'Twenty Dollars	'} onChange={handleTwenty} />
-        <input className="number" type="number" step="100" min="0" placeholder={'One-hundred Dollars'} onChange={handleOneHundred} />
+        Penny: <input className="number" type="number" step="0.01" min="0" placeholder={'Penny'} onChange={handlePenny} />
+        Nickel: <input className="number" type="number" step="0.05" min="0" placeholder={'Nickel'} onChange={handleNickel} />
+        Dime: <input className="number" type="number" step="0.1" min="0" placeholder={'Dime'} onChange={handleDime} />
+        Quarter: <input className="number" type="number" step="0.25" min="0" placeholder={'Quarter'} onChange={handleQuarter} />
+        Dollar: <input className="number" type="number" step="1" min="0" placeholder={'Dollar'} onChange={handleOne} />
+        Five Dollars: <input className="number" type="number" step="5" min="0" placeholder={'Five Dollars'} onChange={handleFive} />
+        Ten Dollars: <input className="number" type="number" step="10" min="0" placeholder={'Ten Dollars'} onChange={handleTen} />
+        Twenty Dollars: <input className="number" type="number" step="20" min="0" placeholder={'Twenty Dollars	'} onChange={handleTwenty} />
+        One-hundred Dollars: <input className="number" type="number" step="100" min="0" placeholder={'One-hundred Dollars'} onChange={handleOneHundred} />
     </div>
   );
 }
