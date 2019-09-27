e6f0fcd70278e9b01c576eb35c56314dc1aabd41
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-09-27 17:26:19 +0300

    Aligning content on the home page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import './HomePage.css';
+import './HomePage.css';
 
 function HomePage() {
   return (
-    <div className="homePage">
+    <div className='HomePage' >
       <h1>Home Page</h1>
-      <div>
-        <Link to='/PalindromeChecker' className='link' >Palindrome Checker</Link>
-      </div>
-      <Link to='/RomanNumeralConverter' className='link' >Roman Numeral Converter</Link>
-      <div>
-        <Link to='/CaesarsCipher' className='link' >Caesars Cipher</Link>
-      </div>
-      <div>
-        <Link to='/TelephoneNumberValidator' className='link' >Telephone Number Validator</Link>
-      </div>
-      <div>
-        <Link to='/cashRegister' className='link' >Cash Register</Link>
+      <div className='Links'>
+        <div>
+          <Link to='/PalindromeChecker' className='link' >Palindrome Checker</Link>
+        </div>
+        <Link to='/RomanNumeralConverter' className='link' >Roman Numeral Converter</Link>
+        <div>
+          <Link to='/CaesarsCipher' className='link' >Caesars Cipher</Link>
+        </div>
+        <div>
+          <Link to='/TelephoneNumberValidator' className='link' >Telephone Number Validator</Link>
+        </div>
+        <div>
+          <Link to='/cashRegister' className='link' >Cash Register</Link>
+        </div>
       </div>
     </div>
   );
