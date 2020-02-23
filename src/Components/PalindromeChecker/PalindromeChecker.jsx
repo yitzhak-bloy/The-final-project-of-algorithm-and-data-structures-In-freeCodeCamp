a8c0c360a5fc0128fa1e81ee15923cebdc7f38e4
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-02-23 10:10:51 +0200

    Convert the PalindromeChecker Component to use hooks instead of Class

--- src/Components/PalindromeChecker/PalindromeChecker.jsx
+++ src/Components/PalindromeChecker/PalindromeChecker.jsx
@@ -1,59 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import InputBox from '../inputBox/InputBox'
 import Button from '../button/button'
 import './PalindromeChecker.css';
 
-class PalindromeChecker extends Component {
-  constructor() {
-      super();
-      this.state = {
-        Sentence: '',
-        IsItPalindrome: ''
-      };
-    }
+const PalindromeChecker = () => {
+  const [sentence, setSentence] = useState('');
+  const [isItPalindrome, setIsItPalindrome] = useState('');
 
-  handleChange = (sentence) => {
-    this.setState({ Sentence: sentence.target.value })
-  }
+  const handleChange = (sentence) => {
+    setSentence(sentence.target.value);
+  };
 
-  handleClick = () => {
-    if (this.state.Sentence.search(/[A-Za-z0-9]/g) >= 0) {
-      const alphanumeric = (this.state.Sentence.match(/[A-Za-z0-9]/g).join('').toLowerCase());
+  const handleClick = () => {
+    if (sentence.search(/[A-Za-z0-9]/g) >= 0) {
+      const alphanumeric = (sentence.match(/[A-Za-z0-9]/g).join('').toLowerCase());
       const alphanumericInReverse = (alphanumeric.split('').reverse().join(''))
-      this.setState({IsItPalindrome: alphanumeric === alphanumericInReverse})   
+      setIsItPalindrome(alphanumeric === alphanumericInReverse);
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="PalindromeChecker">
-        <Link to='/' className='link1 link '  >HomePage</Link>
-        <h1>Palindrome Checker</h1>
-        <p>Checks whether the given string is <a href="https://en.wikipedia.org/wiki/Palindrome" rel="noopener noreferrer" target="_blank" >Palindrome</a> or not.</p>
-        <InputBox 
-          handleChange={this.handleChange} 
-          sentenceWithinTheInputFrame='Write a statement to check' 
-          CaesarsCipher
-        />
-        <Button
-          handleClick={this.handleClick} 
-          WordWrittenInAButton='check' 
-        />
-        { 
-          this.state.IsItPalindrome === true
-          ? 
-            <p className='pBig'>It's a palindrome!</p>
+  return (
+    <div className="PalindromeChecker">
+      <Link to='/' className='link1 link ' >HomePage</Link>
+      <h1>Palindrome Checker</h1>
+      <p>Checks whether the given string is <a href="https://en.wikipedia.org/wiki/Palindrome" rel="noopener noreferrer" target="_blank" >Palindrome</a> or not.</p>
+      <InputBox 
+        handleChange={handleChange} 
+        sentenceWithinTheInputFrame='Write a statement to check' 
+        CaesarsCipher
+      />
+      <Button
+        handleClick={handleClick} 
+        WordWrittenInAButton='check' 
+      />
+      { 
+        isItPalindrome === true
+        ? 
+          <p className='pBig'>It's a palindrome!</p>
+        : 
+          isItPalindrome === false
+          ?       
+            <p className='pBig'>Sorry, that's not a palindrome.</p>
           : 
-            this.state.IsItPalindrome === false
-            ?       
-              <p className='pBig'>Sorry, that's not a palindrome.</p>
-            : 
-              <p>Enter some alpha-numeric characters</p>
-        }
-      </div>
-    );
-  }
+            <p>Enter some alpha-numeric characters</p>
+      }
+    </div>
+  );
 }
 
 export default PalindromeChecker;
