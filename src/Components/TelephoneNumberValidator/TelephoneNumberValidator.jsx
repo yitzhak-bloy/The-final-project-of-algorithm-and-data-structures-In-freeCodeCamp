03ec685848661e7125f9daa95618df4eabc5b0c4
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-02-24 10:04:03 +0200

    Convert the TelephoneNumberValidator Component to use hooks instead of Class

--- src/Components/TelephoneNumberValidator/TelephoneNumberValidator.jsx
+++ src/Components/TelephoneNumberValidator/TelephoneNumberValidator.jsx
@@ -1,60 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import InputBox from '../inputBox/InputBox';
 import Button from '../button/button';
 import './TelephoneNumberValidator.css';
 
-class  TelephoneNumberValidator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      phoneNumber: '',
-      result: ''
-    };
+const TelephoneNumberValidator = () => {
+
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [result, setresult] = useState('');
+
+
+  const handleChange = (phoneNumber) => {
+    setPhoneNumber(phoneNumber.target.value);
   }
 
-  handleChange = (phoneNumber) => {
-    this.setState({ phoneNumber: phoneNumber.target.value })
-  }
-
-  handleClick = () => {
+  const handleClick = () => {
     const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
-    this.setState({ result: regex.test(this.state.phoneNumber)}) 
+    setresult(regex.test(phoneNumber));
   }
 
-  render() {
-    return (
-      <div className="telephoneNumberValidator">
-        <div>
-          <Link to='/' className='link link1' >HomePage</Link>
-        </div>
-        <h1>telephone Number Validator</h1>
-        <p className='spanOfTelephone'>Return true if the passed string looks like a valid US phone number</p>
-        <InputBox 
-          handleChange={this.handleChange} 
-          sentenceWithinTheInputFrame='Write a number to check'
-          isTelephoneNumberValidator
-        />
-        <Button
-          handleClick={this.handleClick} 
-          WordWrittenInAButton='check' 
-        />
-        {
-          this.state.result
-          ?
-            <div>
-              <p className='pBig'>This number is a real phone number!</p>
-            </div>
-          : 
-            this.state.result === ''
-            ?       
-              <p>Enter a phone number for review</p>
-            : 
-              <p className='pBig'>Sorry, this is not a real phone number. Try again.</p>
-        }
+  return (
+    <div className="telephoneNumberValidator">
+      <div>
+        <Link to='/' className='link link1' >HomePage</Link>
       </div>
-    );
-  }
+      <h1>telephone Number Validator</h1>
+      <p className='spanOfTelephone'>Return true if the passed string looks like a valid US phone number</p>
+      <InputBox 
+        handleChange={handleChange} 
+        sentenceWithinTheInputFrame='Write a number to check'
+        isTelephoneNumberValidator
+      />
+      <Button
+        handleClick={handleClick} 
+        WordWrittenInAButton='check' 
+      />
+      {
+        result
+        ?
+          <div>
+            <p className='pBig'>This number is a real phone number!</p>
+          </div>
+        : 
+          result === ''
+          ?       
+            <p>Enter a phone number for review</p>
+          : 
+            <p className='pBig'>Sorry, this is not a real phone number. Try again.</p>
+      }
+    </div>
+  );
 }
 
 export default TelephoneNumberValidator;
