cd4d098d5ccff81ceb8cd91c66b19a89bbb258af
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-02-26 14:40:47 +0200

    homepage design

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -6,20 +6,13 @@ function HomePage() {
   return (
     <div className='HomePage' >
       <h1>Home Page</h1>
+      <p>For more information about the site, see <a href="https://github.com/yitzhak-bloy/freeCodeCamp-JavaScript-Algorithms" rel="noopener noreferrer" target="_blank" >here</a>.</p>
       <div className='Links'>
-        <div>
-          <Link to='/PalindromeChecker' className='link' >Palindrome Checker</Link>
-        </div>
-        <Link to='/RomanNumeralConverter' className='link' >Roman Numeral Converter</Link>
-        <div>
-          <Link to='/CaesarsCipher' className='link' >Caesars Cipher</Link>
-        </div>
-        <div>
-          <Link to='/TelephoneNumberValidator' className='link' >Telephone Number Validator</Link>
-        </div>
-        <div>
-          <Link to='/cashRegister' className='link' >Cash Register</Link>
-        </div>
+        <Link to='/PalindromeChecker' className='link linkHome' >Palindrome Checker</Link>
+        <Link to='/CaesarsCipher' className='link linkHome' >Caesars Cipher</Link>
+        <Link to='/cashRegister' className='link linkHome' >Cash Register</Link>
+        <Link to='/RomanNumeralConverter' className='link linkHome' >Roman Numeral Converter</Link>
+        <Link to='/TelephoneNumberValidator' className='link linkHome' >Telephone Number Validator</Link>
       </div>
     </div>
   );
